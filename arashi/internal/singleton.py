2e73c05965a659bfa3a2ccf2873e4aa36b0c44b8
kifuan <kifuan@foxmail.com>
2023-01-28 20:15:42 +0800

    refactor: add type hints

--- arashi/internal/singleton.py
+++ arashi/internal/singleton.py
@@ -4,11 +4,11 @@ from typing import Optional, Any
 class SingletonMeta(type):
     _singleton_instance: Optional[Any]
 
-    def __init__(cls, *args, **kwargs):
+    def __init__(cls, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         cls._singleton_instance = None
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs) -> Any:
         if not cls._singleton_instance:
             cls._singleton_instance = super().__call__(*args, **kwargs)
         return cls._singleton_instance
